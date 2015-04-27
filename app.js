f8445d95a58ae244400d9e6f15f688ce9ca756e2
Andreas Wissel <a.wissel@proventa.de>
2015-04-27 14:46:13 +0200

    cors header

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var user = require('./routes/user');
 app.use(bodyParser.json());
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://xpense.dev');
+  res.header('Access-Control-Allow-Origin', 'http://localhost');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
